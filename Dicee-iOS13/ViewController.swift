c8634bedbb64efcf1edbce42ee4ef48decec1f48
Abdulkadir Akyurt <akyurt.a@gmx.de>
2023-12-07 17:38:28 +0100

    added int random  methods

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
+        
     }
-
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let randomInt = Int.random(in: 0...5)
+        let randomInt2 = Int.random(in: 0..<6)
+        
+        let imageData = [UIImage(imageLiteralResourceName: "DiceOne"), UIImage(imageLiteralResourceName: "DiceTwo"),
+                         UIImage(imageLiteralResourceName: "DiceThree"),UIImage(imageLiteralResourceName: "DiceFour"),
+                         UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix")]
+        
+        
+        
+        diceImageView1.image = imageData[randomInt]
+        diceImageView2.image = imageData.randomElement()
+        
+        
+        
+    }
+    
+    // end of class
 }
 
